82e5361120830bff03e21234fe0dc27ed617830f
Alex Starchenko <a-starch@yandex-team.ru>
2025-07-27 23:50:44 +0300

    Up deps, compile sdk

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactsScreen.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -46,7 +47,7 @@ fun ContactsScreen(
                 title = { Text(stringResource(R.string.app_name)) },
                 actions = {
                     IconButton(onClick = { showSortBottomSheet = true }) {
-                        Icon(Icons.Default.Sort, contentDescription = stringResource(R.string.menu_sort))
+                        Icon(Icons.AutoMirrored.Filled.Sort, contentDescription = stringResource(R.string.menu_sort))
                     }
                     
                     IconButton(onClick = { showFilterBottomSheet = true }) {
